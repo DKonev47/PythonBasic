8f9dcf3a0cac91869c06de147ce8453f30be9728
DKone <d.konev47@gmail.com>
2024-03-13 22:51:32 +0200

    L2

---
+++ Lesson/L2-2.py
@@ -0,0 +1,11 @@
+digit = int(input("Enter 5 number: "))
+pet = 10000
+ter = 1000
+sto = 100
+ten = 10
+one = 1
+a, b = divmod(digit, ter)
+c, d = divmod(b, sto)
+e, f = divmod(a, ten)
+g, h = divmod(d, ten)
+print((h * pet) + (g * ter) + (c * sto) + (f * ten) + (e * one))
